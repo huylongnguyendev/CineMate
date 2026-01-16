bdb21d85dffeb7e5725702b10d6d0f524911dac3
Nguyen Huy Long <nguyenhuylong.dev@gmail.com>
2026-01-16 17:46:41 +0700

    update sections

--- src/components/sections/movie/MovieSectionItem.tsx
+++ src/components/sections/movie/MovieSectionItem.tsx
@@ -8,9 +8,12 @@ export default function MovieSectionItem({ item }: { item: ResultType }) {
     <>
       <Link
         href={`/movies/${item.id}`}
-        className="block bg-card rounded-md p-4 space-y-2"
+        className="block bg-card rounded-md p-4 space-y-2 group"
       >
         <div className="relative w-full h-40">
+          <div
+            className="absolute size-full z-10 bg-black/0 transitions group-hover:bg-black/50"
+          />
           <Image
             src={smallImgURL + item.poster_path}
             alt={item.title || item.original_title}
@@ -18,7 +21,9 @@ export default function MovieSectionItem({ item }: { item: ResultType }) {
             className="object-cover rounded-md"
           />
         </div>
-        <h3 className="font-semibold text-card-foreground truncate">{item.title || item.original_title}</h3>
+        <h3 className="font-semibold text-card-foreground truncate transitions group-hover:text-primary">
+          {item.title || item.original_title}
+        </h3>
         <p className="text-sm text-muted-foreground">{item.release_date.slice(0, 4)}</p>
       </Link>
     </>
--- src/components/sections/show/ShowSectionItem.tsx
+++ src/components/sections/show/ShowSectionItem.tsx
@@ -8,9 +8,12 @@ export default function ShowSectionItem({ item }: { item: ResultType }) {
     <>
       <Link
         href={`/movies/${item.id}`}
-        className="block bg-card rounded-md p-4 space-y-2"
+        className="block bg-card rounded-md p-4 space-y-2 group"
       >
         <div className="relative w-full h-40">
+          <div
+            className="absolute size-full z-10 bg-black/0 transitions group-hover:bg-black/50"
+          />
           <Image
             src={smallImgURL + item.poster_path}
             alt={item.name || item.original_name}
@@ -18,7 +21,7 @@ export default function ShowSectionItem({ item }: { item: ResultType }) {
             className="object-cover rounded-md"
           />
         </div>
-        <h3 className="font-semibold text-card-foreground truncate">{item.name || item.original_name}</h3>
+        <h3 className="font-semibold text-card-foreground truncate transitions group-hover:text-primary">{item.name || item.original_name}</h3>
         <p className="text-sm text-muted-foreground">{item.first_air_date.slice(0, 4)}</p>
       </Link>
     </>
